b1c65831aa6511b0624fb6e1593b6cf6319b7555
Ozkan Yilmaz <ozkanyilmaz@gmail.com>
2023-09-11 10:30:43 +0200

    add repository

--- src/ProductApi/ProductApi.Domain/Interfaces/IBaseRepository.cs
+++ src/ProductApi/ProductApi.Domain/Interfaces/IBaseRepository.cs
@@ -6,13 +6,9 @@ namespace ProductApi.Domain.Interfaces
     {
         Task<T> GetByIdAsync(string id);
         Task<IList<T>> ListAllAsync();
-        //Task<IList<T>> ListAsync(ISpecification<T> spec);
-        //Task<T?> FirstOrDefaultAsync(ISpecification<T?> spec);
         Task<T> AddAsync(T entity);
         void Update(T entity);
         void Delete(T entity);
-        //Task<int> CountAsync(ISpecification<T> spec);
-
         Task<int> SaveChangesAsync();
     }
 }
---
+++ src/ProductApi/ProductApi.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Domain.Interfaces;
+using ProductApi.Domain.Models;
+using ProductApi.Infrastructure.Data;
+
+namespace ProductApi.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        protected readonly ProductDbContext _dbContext;
+
+        public BaseRepository(ProductDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual async Task<T> GetByIdAsync(string id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IList<T>> ListAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            return entity;
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
